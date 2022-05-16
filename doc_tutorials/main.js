b90894f4a3dc6c58835fbd8a8f1f848756dd7393
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-05-16 15:34:40 +0500

    v-show, v-bind, v-if/else-if/else

--- doc_tutorials/main.js
+++ doc_tutorials/main.js
@@ -2,9 +2,8 @@ const App = Vue.createApp({
     data() {
         return {
             product: "Socks",
-            description: "Just Vue Socks",
             image: "./assets/img/blue_socks.png",
-            url: "https://google.com"
+            inventory: 20
         }
     }
 })
