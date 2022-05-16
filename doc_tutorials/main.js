cddf6fcefcb4fcb0bf7564f89b3e2a02676d8707
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-05-16 16:32:43 +0500

    v-on(click, mouseover)

--- doc_tutorials/main.js
+++ doc_tutorials/main.js
@@ -4,13 +4,20 @@ const App = Vue.createApp({
             product: "Socks",
             image: "./assets/img/blue_socks.png",
             inventory: 20,
-            onSale: true,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                {id: 1, color: "blue"},
-                {id: 2, color: "green"},
+                {id: 1, color: "blue", image: "./assets/img/blue_socks.png"},
+                {id: 2, color: "green", image: "./assets/img/green_socks.png"},
             ],
-            sizes: ["X", "XL", "XXL"]
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(image) {
+            this.image = image
         }
     }
 })
