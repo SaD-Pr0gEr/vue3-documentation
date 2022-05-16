26c547e1a741e87e82be3da1d128312bb4386bfa
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-05-16 16:15:01 +0500

    v-for

--- doc_tutorials/main.js
+++ doc_tutorials/main.js
@@ -4,7 +4,12 @@ const App = Vue.createApp({
             product: "Socks",
             image: "./assets/img/blue_socks.png",
             inventory: 20,
-            onSale: true
+            onSale: true,
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
+                {id: 1, color: "blue"},
+                {id: 2, color: "green"},
+            ]
         }
     }
 })
