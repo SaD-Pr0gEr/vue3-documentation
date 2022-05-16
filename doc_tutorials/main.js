a92f486497eb3ed56f1c6a18d81bc41425c05940
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-05-16 22:44:02 +0300

    computed properties

--- doc_tutorials/main.js
+++ doc_tutorials/main.js
@@ -2,28 +2,39 @@ const App = Vue.createApp({
     data() {
         return {
             product: "Socks",
-            image: "./assets/img/blue_socks.png",
-            inventory: 20,
+            brand: "Vue company",
+            selectedVariantIndex: 0,
+            // inventory: 20,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                {id: 1, color: "blue", image: "./assets/img/blue_socks.png"},
-                {id: 2, color: "green", image: "./assets/img/green_socks.png"},
+                {id: 1, color: "blue", image: "./assets/img/blue_socks.png", quantity: 20},
+                {id: 2, color: "green", image: "./assets/img/green_socks.png", quantity: 0},
             ],
             cart: 0,
-            inStock: false
         }
     },
     methods: {
         addToCart() {
             this.inStock ? this.cart += 1 : null
         },
-        updateImage(image) {
-            this.image = image
-        },
         removeFromCart() {
             if(this.cart >= 1){
                 this.cart -= 1
             }
+        },
+        updateVariant(productIndex) {
+            this.selectedVariantIndex = productIndex
+        }
+    },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`
+        },
+        showProductImage() {
+            return this.variants[this.selectedVariantIndex].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariantIndex].quantity
         }
     }
 })
